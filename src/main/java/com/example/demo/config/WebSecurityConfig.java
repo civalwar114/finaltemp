7f82d1a8b870d4a678f96ed7dea2cf118b7cd2e2
civalwar114 <sly114@naver.com>
2022-07-14 11:37:34 +0900

    패키지추가
    by 유림&승준

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -34,7 +34,7 @@ public class WebSecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests() // /와 /auth/** 경로는 인증 안해도 됨.
-                .antMatchers("/", "/auth/**","/saram/**","/travinfo/**").permitAll()
+                .antMatchers("/", "/auth/**","/saram/**","/travinfo/**").permitAll() //여기의 모든 경로는 로그인을 안해도 허용
                 .anyRequest() // /와 /auth/**이외의 모든 경로는 인증 해야됨.
                 .authenticated();
 
